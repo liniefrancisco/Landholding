d1a4e8f37c83bdab24242914f651bc45a6ecae11
liniefrancisco <liniefrancisco01@gmail.com>
2025-06-24 14:29:00 +0800

    1

--- application/logs/log-2025-06-24.php
+++ application/logs/log-2025-06-24.php
@@ -2,3 +2,5 @@
 
 ERROR - 2025-06-24 10:35:38 --> Severity: error --> Exception: Unable to locate the model you have specified: Land_model C:\laragon\www\landholding\system\core\Loader.php 348
 ERROR - 2025-06-24 10:36:51 --> 404 Page Not Found: Legal_f/rpt
+ERROR - 2025-06-24 14:20:33 --> 404 Page Not Found: Rpt/Rpt_tbl
+ERROR - 2025-06-24 14:26:56 --> 404 Page Not Found: Rpt/Rpt_tbl
