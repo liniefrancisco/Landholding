f69019d61a8b5da091f7abfd72cc4ecfaffca551
liniefrancisco <liniefrancisco01@gmail.com>
2025-06-24 10:36:36 +0800

    merge linie_local RPT changes

---
+++ application/logs/log-2025-06-24.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-06-24 10:35:38 --> Severity: error --> Exception: Unable to locate the model you have specified: Land_model C:\laragon\www\landholding\system\core\Loader.php 348
