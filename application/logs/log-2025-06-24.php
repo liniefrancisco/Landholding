30234b7c27c5e1c79dbb12dd67fdcea6be6c7e93
liniefrancisco <liniefrancisco01@gmail.com>
2025-06-24 10:52:36 +0800

    06-24-25

--- application/logs/log-2025-06-24.php
+++ application/logs/log-2025-06-24.php
@@ -1,3 +1,4 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2025-06-24 10:35:38 --> Severity: error --> Exception: Unable to locate the model you have specified: Land_model C:\laragon\www\landholding\system\core\Loader.php 348
+ERROR - 2025-06-24 10:36:51 --> 404 Page Not Found: Legal_f/rpt
